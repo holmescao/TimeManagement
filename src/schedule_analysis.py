55d6f43cf5905f2796ab5642187576e1895ae3cb
holmescao <holmescaoxy@gmail.com>
2021-04-14 21:40:26 +0800

    add real use time record

--- src/schedule_analysis.py
+++ src/schedule_analysis.py
@@ -2,7 +2,7 @@
 Author: Holmescao
 Date: 2021-03-16 12:57:18
 LastEditors: Holmescao
-LastEditTime: 2021-04-14 21:31:24
+LastEditTime: 2021-04-14 21:39:46
 Description: 自动分析schedule文档信息，用于个人时间分析与管理
 RunTime：9 sec
 '''
